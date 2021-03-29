96000914277a138ae2ce636d5bfd095bb5bcd41f
Daniel Amado <danielfep.am@gmail.com>
2021-03-28 20:57:28 -0500

    [UPDATE] FLUSH PRIVILEGES

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,4 +3,5 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-Grant SELECT PRIVILEGES ON performance_schema TO 'hbnb_test'@'localhost';
+GRANT SELECT PRIVILEGES ON performance_schema TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
