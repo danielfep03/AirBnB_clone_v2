e8503e45cc59e2dcd781abfa02689abb4ffcbaa8
7honarias <7honarias@gmail.com>
2021-03-29 00:48:57 +0000

    [UPDATE] test for state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ """
 from tests.test_models.test_base_model import test_basemodel
+from models.base_model import BaseModel
 from models.state import State
-
+import unittest
 
 class test_state(test_basemodel):
     """ """
@@ -17,3 +18,13 @@ class test_state(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+
+class MyTestCase(unittest.TestCase):
+    """Test State"""
+    def test_class(self):
+        """Test is Class"""
+        my_state = State()
+        setattr(my_state, 'name', 'California')
+        self.assertIsInstance(my_state, BaseModel)
+        self.assertEqual(my_state.name, "California")
