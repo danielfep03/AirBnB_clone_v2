e3a10d8dc88412d13f421e732a2f51132a93b307
Daniel Amado <danielfep.am@gmail.com>
2021-03-29 23:27:45 -0500

    [UPDATE] typo fixed

--- models/user.py
+++ models/user.py
@@ -7,10 +7,10 @@ from sqlalchemy import Column, String
 class User(BaseModel):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    email = Column(string(128) nullable=False)
-    password = Column(string(128) nullable=False)
-    first_name = Column(string(128) nullable=False)
-    last_name = Column(string(128) nullable=False)
+    email = Column(string(128), nullable=False)
+    password = Column(string(128), nullable=False)
+    first_name = Column(string(128))
+    last_name = Column(string(128))
 
     def __init__(self, *args, **kwargs):
         """ Initialitation """
