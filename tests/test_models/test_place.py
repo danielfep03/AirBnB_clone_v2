735b77d6406144d92543a67cba52be543dbc3b9e
Daniel Amado <danielfep.am@gmail.com>
2021-03-30 23:32:54 -0500

    [UPDATE] Added test cases

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,89 +1,23 @@
 #!/usr/bin/python3
-"""test for place"""
-from tests.test_models.test_base_model import test_basemodel
-from models.base_model import BaseModel
-from models.place import Place
+"""Unittest for Place """
 import unittest
-import pep8
 import os
-
-
-class test_Place(test_basemodel):
-    """ """
-
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Place"
-        self.value = Place
-
-    def test_city_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.city_id), str)
-
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
-
-    def test_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
-
-    def test_description(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.description), str)
-
-    def test_number_rooms(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.number_rooms), int)
-
-    def test_number_bathrooms(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.number_bathrooms), int)
-
-    def test_max_guest(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.max_guest), int)
-
-    def test_price_by_night(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.price_by_night), int)
-
-    def test_latitude(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.latitude), float)
-
-    def test_longitude(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.latitude), float)
-
-    def test_amenity_ids(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.amenity_ids), list)
+from models.place import Place
+from models.base_model import BaseModel
+import pep8
 
 
 class TestPlace(unittest.TestCase):
-    """this will test the place class"""
+    """ create class test """
 
     @classmethod
     def setUpClass(cls):
-        """set up for test"""
+        """ test """
         cls.place = Place()
         cls.place.city_id = "1234-abcd"
         cls.place.user_id = "4321-dcba"
-        cls.place.name = "Death Star"
-        cls.place.description = "UNLIMITED POWER!!!!!"
+        cls.place.name = "Holberton"
+        cls.place.description = "disponibleeee"
         cls.place.number_rooms = 1000000
         cls.place.number_bathrooms = 1
         cls.place.max_guest = 607360
@@ -94,28 +28,28 @@ class TestPlace(unittest.TestCase):
 
     @classmethod
     def teardown(cls):
-        """at the end of the test this will tear it down"""
+        """ del """
         del cls.place
 
     def tearDown(self):
-        """teardown"""
+        """ read file """
         try:
             os.remove("file.json")
         except Exception:
             pass
 
     def test_pep8_Place(self):
-        """Tests pep8 style"""
+        """Testing pep8 """
         style = pep8.StyleGuide(quiet=True)
         p = style.check_files(['models/place.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+        self.assertEqual(p.total_errors, 0, "pep8 errors")
 
     def test_checking_for_docstring_Place(self):
-        """checking for docstrings"""
+        """ check places docstrings"""
         self.assertIsNotNone(Place.__doc__)
 
     def test_attributes_Place(self):
-        """chekcing if amenity have attributes"""
+        """ testing place"""
         self.assertTrue('id' in self.place.__dict__)
         self.assertTrue('created_at' in self.place.__dict__)
         self.assertTrue('updated_at' in self.place.__dict__)
@@ -132,11 +66,11 @@ class TestPlace(unittest.TestCase):
         self.assertTrue('amenity_ids' in self.place.__dict__)
 
     def test_is_subclass_Place(self):
-        """test if Place is subclass of Basemodel"""
+        """ test subclass pa """
         self.assertTrue(issubclass(self.place.__class__, BaseModel), True)
 
     def test_attribute_types_Place(self):
-        """test attribute type for Place"""
+        """ place atributes"""
         self.assertEqual(type(self.place.city_id), str)
         self.assertEqual(type(self.place.user_id), str)
         self.assertEqual(type(self.place.name), str)
@@ -149,13 +83,16 @@ class TestPlace(unittest.TestCase):
         self.assertEqual(type(self.place.longitude), float)
         self.assertEqual(type(self.place.amenity_ids), list)
 
+    @unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') == 'db',
+        "This test only work in Filestorage")
     def test_save_Place(self):
         """test if the save works"""
         self.place.save()
         self.assertNotEqual(self.place.created_at, self.place.updated_at)
 
     def test_to_dict_Place(self):
-        """test if dictionary works"""
+        """testing"""
         self.assertEqual('to_dict' in dir(self.place), True)
 
 
