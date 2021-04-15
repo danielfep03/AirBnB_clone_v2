4dbe766c3ad26d119f3cda1dea6b3489b6ddb6ee
7honarias <7honarias@gmail.com>
2021-04-15 20:19:59 +0000

    [ADD] - correction of 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,9 +3,10 @@
 sudo apt-get update -y
 sudo apt-get install nginx -y
 
-sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-echo "Holberton School" > '/data/web_static/releases/test/index.html'
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i "42i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+chown -R ubuntu:ubuntu /data/
+sudo sed -i "42i\\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx start
