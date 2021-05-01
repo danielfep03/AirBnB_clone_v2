d70cd1c2ff847d3ac03a9f465c5ffd24cd5a9757
Daniel Amado <danielfep.am@gmail.com>
2021-05-01 18:41:33 -0500

    [ADDED] Added new route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' Module that starts a Flask web application '''
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def main():
+    ''' Main page Content '''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    ''' Content Route hbnb '''
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
