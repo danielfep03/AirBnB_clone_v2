4f8339e865190d7173f0ad5cb79cc77116016775
Daniel Amado <danielfep.am@gmail.com>
2021-05-01 19:08:38 -0500

    [ADDED] Custom routes

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+''' Module that starts a Flask web application '''
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def main():
+    ''' Main page Content '''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    ''' Content Route hbnb '''
+    return 'HBNB'
+
+
+@app.route('/c/<route>')
+def custom_route(route):
+    route = 'C ' + route
+    return route.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
