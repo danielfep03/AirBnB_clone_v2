924dbf27132b754e0f86e8d4ad559adf4c0e22ae
Daniel Amado <danielfep.am@gmail.com>
2021-05-02 23:48:02 -0500

    [ADDED] HTML template

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+''' Module that starts a Flask web application '''
+
+from flask import Flask, abort, render_template
+from models.state import State
+from models.city import City
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def main():
+    ''' Main page Content '''
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def close(states):
+    ''' Close the connection with db '''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
